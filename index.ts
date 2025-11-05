96cf4bf2c26df86dfa525ce8b35ad5da3ca8b708
dell-optiplex-790 <dell.optiplex.d4719126@gmail.com>
2025-11-05 20:36:44 +0100

    add uppercase registers and documentation

--- index.ts
+++ index.ts
@@ -3,22 +3,36 @@
 
     for(var i = 0; i < code.length; i++) {
         if(code[i] == '$') {
-            var chunk: string = code.slice(i+1, i+4);
-            if(!(/[a-z]{1}([a-f0-9]{2}|\[\])/.test(chunk) && chunk.length == 3)) {
+            var chunk: string = code.slice(i+1, i+6);
+            if(!(/[a-zA-Z]{1}([a-f0-9]{2}|\[([a-f0-9]{2})?\])/).test(chunk)) {
                 return {success: false};
             }
             if(chunk.slice(1,3) == '[]') {
                 js.stack[chunk[0]] = [];
-            } else {
+            } else if(/[a-zA-Z]{1}[a-f0-9]{2}/.test(chunk)) {
                 js.stack[chunk[0]] = parseInt(chunk.slice(1,3), 16);
+            } else {
+                var len: number = parseInt(chunk.slice(2,4), 16);
+                var arr: Array<number> = [];
+                for(var i = 0; i < len; i++) {
+                    arr.push(0);
+                }
+                js.stack[chunk[0]] = arr;
             }
             i += 3;
         } else if(code[i] == '.') {
             var chunk = code.slice(i, i+2);
-            if(!(/\.[a-z]{1}/.test(chunk) && chunk.length == 2)) {
+            if(!(/\.[a-zA-Z]{1}/.test(chunk) && chunk.length == 2)) {
                 return {success: false};
             }
-            js.code += '$=' + chunk[1] + ';'
+            js.code += `$=${chunk[1]};`;
+            i++
+        } else if(code[i] == '_') {
+            var chunk = code.slice(i, i+2);
+            if(!(/_[a-zA-Z]{1}/.test(chunk) && chunk.length == 2)) {
+                return {success: false};
+            }
+            js.code += `console.log(${chunk[1]}.map(function(e){return String.fromCharCode(e)}).join(''));`;
             i++
         } else if(code[i] == '>') {
             js.code += '_++;'
@@ -29,24 +43,24 @@
             if(/#[a-f0-9]{2}/.test(chunk)) {
                 js.code += `$[_]=${parseInt(chunk.slice(1,3), 16).toString()};`;
                 i += 2;
-            } else if(/#[a-z]{1}/.test(chunk)) {
+            } else if(/#[a-zA-Z]{1}/.test(chunk)) {
                 js.code += `$[_]=${chunk[1]};`;
                 i++;
             } else {
                 return {success: false};
             }
         } else if('+-*/^'.indexOf(code[i]) != -1) {
-            var chunk: string = code.slice(i+1, i+3).split('').filter(e=>'{}'.indexOf(e)==-1).join('');
+            var chunk: string = code.slice(i+1, i+3).split('').filter(e=>'{}+-*/^'.indexOf(e)==-1).join('');
             if(!(/[a-f0-9]{2}/.test(chunk))) {
-                js.code += `$[_]${code[i]}=${chunk};`;
+                js.code += `var _v=${chunk};if(typeof $=='number'){$${code[i]}=_v}else{$[_]${code[i]}=_v}`;
                 i++;
             } else if(!(/[a-z]{1}/.test(chunk))) {
                 return {success: false};
             } else {
-                js.code += `$[_]${code[i]}=${parseInt(chunk.slice(1,3), 16).toString()};`
+                js.code += `var _v=${parseInt(chunk.slice(1,3), 16).toString()};if(typeof $=='number'){$${code[i]}=_v}else{$[_]${code[i]}=_v}`
                 i += 3;
             }
-        } else if(/[a-z]:\{/.test(code.slice(i-1,i+2))) {
+        } else if(/[a-zA-Z]:\{/.test(code.slice(i-1,i+2))) {
             js.code += `$=${code[i-1]};for(_=0;_<$.length;_++){`;
             i++
         } else if(code[i] == '}') {
